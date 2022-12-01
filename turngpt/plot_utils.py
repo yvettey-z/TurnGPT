5eadc5add8a8b66d72e471ee3b0295f55781bcd0
DodoY_529 <64227800+yvettey-z@users.noreply.github.com>
2022-12-01 16:54:01 -0500

    Update plot_utils.py

--- turngpt/plot_utils.py
+++ turngpt/plot_utils.py
@@ -29,6 +29,7 @@ def plot_trp(
     if proj is not None:
         proj_argmax = torch.argmax(proj, dim=1)
         ax.bar(x, proj_argmax, alpha=0.1, width=1.0, color="b", label="projection")
+    #   ax.bar(x, proj, alpha=0.1, width=1.0, color="b", label="projection")
     ax.bar(x, trp, width=0.3, color="b", label="TRP")
     ax.set_xticks(x)
     if text is not None:
