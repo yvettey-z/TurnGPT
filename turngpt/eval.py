c23839f350c5e9aee70cc4eec0d0bbe45bf99d7c
akonoroshi <akr.oedo@gmail.com>
2022-12-08 09:19:53 -0500

    Handling ZeroDivisionError

--- turngpt/eval.py
+++ turngpt/eval.py
@@ -1,17 +1,24 @@
 from nltk.translate import bleu_score, meteor_score, nist_score
 from typing import Dict, List
 
+def zeroDivisionWrapper(func, references, hypothesis, **kwargs):
+    try:
+        score = func(references, hypothesis, **kwargs)
+    except ZeroDivisionError:
+        score = 0.
+    return score
+
 def calc_score(references, hypothesis) -> Dict[str, float]:
     '''
     Returns scores for language generation. Refer to NLTK documentation for the types 
     of `references` and `hypothesis`
     '''
     scores = {}
-    scores['BLEU-2'] = bleu_score.sentence_bleu(references, hypothesis, (0.5, 0.5))
-    scores['BLEU-4'] = bleu_score.sentence_bleu(references, hypothesis, (0.25, 0.25, 0.25, 0.25))
-    scores['METEOR'] = meteor_score.meteor_score(references, hypothesis)
-    scores['NIST-2'] = nist_score.sentence_nist(references, hypothesis, 2)
-    scores['NIST-4'] = nist_score.sentence_nist(references, hypothesis, 4)
+    scores['BLEU-2'] = zeroDivisionWrapper(bleu_score.sentence_bleu, references, hypothesis, weights=(0.5, 0.5))
+    scores['BLEU-4'] = zeroDivisionWrapper(bleu_score.sentence_bleu, references, hypothesis, weights=(0.25, 0.25, 0.25, 0.25))
+    scores['METEOR'] = zeroDivisionWrapper(meteor_score.meteor_score, references, hypothesis)
+    scores['NIST-2'] = zeroDivisionWrapper(nist_score.sentence_nist, references, hypothesis, n=2)
+    scores['NIST-4'] = zeroDivisionWrapper(nist_score.sentence_nist, references, hypothesis, n=4)
     return scores
 
 def process_for_nltk(sentence: str, tokenizer) -> List[str]:
